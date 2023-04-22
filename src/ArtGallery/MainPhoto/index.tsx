961203e764aece4e0936faeeb7485285e8d08631
Andriikryl <andreyk557@gmail.com>
2023-04-22 18:42:13 +0300

    feat[MainPhoto-component-jsx-style-module.css]

--- src/ArtGallery/MainPhoto/index.tsx
+++ src/ArtGallery/MainPhoto/index.tsx
@@ -1,15 +1,37 @@
 import { CommonClassProps, Photo } from "../types";
-
+import cl from "classnames";
+import style from "./index.module.css";
 interface MainPhotoProps extends CommonClassProps {
-    prevPhoto?: Photo;
-    activePhoto: Photo;
-    nextPhoto?: Photo;
+  prevPhoto?: Photo;
+  activePhoto: Photo;
+  nextPhoto?: Photo;
 }
 
-export const MainPhoto: React.FC<MainPhotoProps> = ({ 
-    prevPhoto,
-    activePhoto,
-    nextPhoto,
- }) => (
-    <div>Main Photo</div>
- );
+export const MainPhoto: React.FC<MainPhotoProps> = ({
+  prevPhoto,
+  activePhoto,
+  nextPhoto,
+  className,
+}) => (
+  <div className={cl(className, style.mainPhoto)}>
+    {prevPhoto && (
+      <img
+        src={prevPhoto.src}
+        alt={prevPhoto.description}
+        className={style.mainPhotoImagePrev}
+      />
+    )}
+    <img
+      src={activePhoto.src}
+      alt={activePhoto.description}
+      className={style.mainPhotoImage}
+    />
+    {nextPhoto && (
+      <img
+        src={nextPhoto.src}
+        alt={nextPhoto.description}
+        className={style.mainPhotoImageNext}
+      />
+    )}
+  </div>
+);
