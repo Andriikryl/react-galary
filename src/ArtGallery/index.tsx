caacef1add71659b15829944b49fd2f21f708785
Andriikryl <andreyk557@gmail.com>
2023-04-22 16:47:31 +0300

    feat[art-galary-tsx]

--- src/ArtGallery/index.tsx
+++ src/ArtGallery/index.tsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import { Photo } from "./types";
+import style from "./index.module.css";
 
 interface ArtGalleryProps {
   photos: Photo[];
@@ -9,5 +10,38 @@ export const ArtGallery: React.FC<ArtGalleryProps> = ({ photos }) => {
   if (!photos.length) {
     return null;
   }
-  return <div>My gallery</div>;
+
+  const [indexActivePhoto, setIndexActivePhoto] = useState(0);
+  const activePhoto = photos[indexActivePhoto];
+  const prevPhoto = photos[indexActivePhoto - 1];
+  const nextPhoto = photos[indexActivePhoto + 1];
+
+  return (
+    <div className={style.gallery}>
+      <div className="style.container">
+        <MainPhoto
+          prevPhoto={prevPhoto}
+          currentPhoto={activePhoto}
+          nexPhoto={nexPhoto}
+          className={style.mainPhoto}
+        />
+        <Nuvigation
+          className={style.navigation}
+          disabledPrev={!prevPhoto}
+          disabledNext={!nextPhoto}
+          onPrevClick={() => {
+            setIndexActivePhoto(indexActivePhoto - 1);
+          }}
+          onNextClick={() => {
+            setIndexActivePhoto(indexActivePhoto + 1);
+          }}
+        />
+      </div>
+      <PreviewGallery
+        activePhotoIndex={indexActivePhoto}
+        photos={photos}
+        className={style.PreviewList}
+      />
+    </div>
+  );
 };
